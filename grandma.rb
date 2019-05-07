689fa8aa216fb5f8fc06cbed3e74bb049ddc7ed1
Pascal Rassaby <pasbynumbers@gmail.com>
2019-05-07 03:35:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,9 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
   if (string == "I LOVE YOU GRANDMA!" )
-    return 
+    return "I LOVE YOU TOO PUMPKIN!"
+    elsif (string == string.upcase)
+    return ""
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
