c654ff1d20d848dd3287b6edb15c95c668b259a6
Pascal Rassaby <pasbynumbers@gmail.com>
2019-05-07 03:36:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,6 +3,8 @@ def speak_to_grandma(string)
   if (string == "I LOVE YOU GRANDMA!" )
     return "I LOVE YOU TOO PUMPKIN!"
     elsif (string == string.upcase)
+    return "NO, NOT SINCE 1938!"
+  else
     return ""
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
